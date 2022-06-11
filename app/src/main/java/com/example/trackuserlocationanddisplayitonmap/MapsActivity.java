bad1c5dc2b54153dc4963231bddeb8ba50d4273c
Faris Beginovic <far.beg@web.de>
2022-06-11 17:47:50 +0200

    Compile SDK version des Projekts auf API Level 31 zurück gesetzt weil ich es doch ohne den GeocodeListener machen möchte, weil der Lsitener erst seit der neusten API (API level 33) dabei ist und nicht viele Geräte das unterstützen (Momentan nur ~6,7% der Android Geräte weltweit) . Die App funktioniert jetzt. Ich werde mir die Funktionsweise des

--- app/src/main/java/com/example/trackuserlocationanddisplayitonmap/MapsActivity.java
+++ app/src/main/java/com/example/trackuserlocationanddisplayitonmap/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.example.trackuserlocationanddisplayitonmap;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -14,6 +16,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,6 +26,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.trackuserlocationanddisplayitonmap.databinding.ActivityMapsBinding;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -31,8 +36,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
     Location lastKnownLocation;
+    String address;
 
-    //Methode nur zum Testing benutzt, nicht relevant.
+    //Methode just used for some testing, not relevant to the actual code
     public Location getMockLocation(double latitude, double longitude) {
         Location location = new Location(LocationManager.GPS_PROVIDER);
         location.setLatitude(latitude);
@@ -69,13 +75,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        Geocoder.GeocodeListener geocodeListener = new Geocoder.GeocodeListener() {
-            @Override
-            public void onGeocode(@NonNull List<Address> list) {
-
-            }
-        };
-
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         LocationListener locationListener = new LocationListener() {
             @Override
@@ -86,27 +85,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Your position"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,10f));
 
-                Log.i("MyInfo","Ich wurde ausgeführt 3");
+                //The app displays a Toast with the current/changed address info
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(userLocation.latitude, userLocation.longitude, 1);
+                    if(addresses != null && addresses.size() >= 1)
+                    address = addresses.get(0).getAddressLine(0);
+                    Toast.makeText(getApplicationContext(), address,LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         };
 
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED){
             requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
         } else {
-            Log.i("MyInfo", "User already gave Permission:");
             lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            Log.i("MyInfo", "Ich wurde ausgeführt 1");
         }
 
-        lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        Log.i("MyInfo", "Ich wurde ausgeführt 2");
-        // Add a marker in Sydney and move the camera
+        // Add a marker to Users current/last-known position and move the camera
             LatLng userLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
             mMap.addMarker(new MarkerOptions().position(userLocation).title("Your position"));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
 
+        //If the location of the user changes for at least 10 meters, the location gets updated and onLocationChanged in the locationListener gets called.
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,10, locationListener);
 
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 600,1, locationListener);
+
     }
 }
 
@@ -116,4 +121,7 @@ startet, sondern mit dem "Apply Changes and restart Activity"-Button. Mir ist no
 App auch nicht mehr mit dem Run-Button starten. Um das aber zu fixen, kann man einfach random irgendeine Permission im Manifest hinzufügen
 oder rausnehmen, dann lässt sich die App wieder mit "Run"-Button starten. Aber den "Apply Changes and restart Activity"-Button kann man immernoch
 nicht benutzen
+
+Mittlerweile benutzt man einen GeocodeListener (Seit API 33), wenn man mit Adressen arbeitet. Habe es jetzt aber ohne gemacht, weil man wenig Material im Internet dazu findet. Scheint aber nicht
+viel schwieriger zu sein.
  */
